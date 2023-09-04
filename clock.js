11cf2222d903e14e25defd748456c5ed80ae0149
n00k1e <plotnikoff1488@gmail.com>
2023-09-04 18:18:00 +0300

    created info bar

---
+++ clock.js
@@ -0,0 +1,30 @@
+function Time(){
+    let date = new Date();
+    let sec = date.getSeconds();
+    let min = date.getMinutes();
+    let hour = date.getHours();
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+
+    if(hour === 0){
+        hour = 24
+    };
+
+    if(hour > 24){
+        hour = hour - 24;
+    };
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    let time = hour + ":" + min + ":" + sec;
+    let fullDate = day + "/" + month + "/" + year;
+    let t = setTimeout(function() { Time() }, 1000);
+
+    document.getElementById("time").innerText = time;
+    document.getElementById("date").innerText = fullDate;
+};
+
+Time();
